1d4ea7cb370293f4e2a4c1b208e5e396ab291eeb
lingdai0012 <ling_dai@outlook.com>
2022-04-08 00:45:00 +0200

    w5_1

--- Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -1,9 +1,18 @@
 # Uses python3
 import sys
+import math
 
 def get_change(m):
     #write your code here
-    return m // 4
+    min_num_coins = [math.inf]*(m+1)
+    min_num_coins[0] = 0
+    for i in range(1, m+1):
+        for coin in [1, 3, 4]:
+            if i >= coin:
+                num_coins = min_num_coins[i-coin] + 1
+                if num_coins < min_num_coins[i]:
+                    min_num_coins[i] = num_coins
+    return min_num_coins[m]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
