345e05bbb2b20dc751c8d5f99d056216998d99b9
lingdai0012 <ling_dai@outlook.com>
2022-04-08 01:17:19 +0200

    c1w5_2

--- Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -1,17 +1,21 @@
 # Uses python3
 import sys
+import math
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
-        else:
-            n = n - 1
-    return reversed(sequence)
+    min_num_operations = [0, 0]
+    optimal_sequences = [[], [1]]
+    for i in range(2, n+1):
+        num_operations_3 = min_num_operations[i//3] + 1 if i%3==0 else math.inf
+        num_operations_2 = min_num_operations[i//2] + 1 if i%2==0 else math.inf
+        num_operations_1 = min_num_operations[i-1] + 1
+        possible_numbers = [num_operations_3, num_operations_2, num_operations_1]
+        next_to_add = [optimal_sequences[i//3], optimal_sequences[i//2], optimal_sequences[i-1]]
+        min_number = min(possible_numbers)
+        next_sequence = next_to_add[possible_numbers.index(min_number)] + [i]
+        min_num_operations.append(min_number)
+        optimal_sequences.append(next_sequence )
+    return optimal_sequences[n]
 
 input = sys.stdin.read()
 n = int(input)
