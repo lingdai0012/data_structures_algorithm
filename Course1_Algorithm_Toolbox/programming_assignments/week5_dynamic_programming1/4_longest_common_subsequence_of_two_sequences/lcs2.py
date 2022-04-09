31531ab3eed01d20e292d02f0562323271b1922d
lingdai0012 <ling_dai@outlook.com>
2022-04-09 11:19:49 +0200

    c1w5_5

--- Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
+++ Course1_Algorithm_Toolbox/programming_assignments/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences/lcs2.py
@@ -1,14 +1,23 @@
 #Uses python3
 
 import sys
+import numpy as np
 
 def lcs2(a, b):
     #write your code here
-    return min(len(a), len(b))
-
+    a_len = len(a)
+    b_len = len(b)
+    number_commom_sequences = np.zeros(shape=(a_len+1, b_len+1), dtype=int)
+    for ii in range(1, a_len+1):
+        for jj in range(1, b_len+1):
+            if a[ii-1] == b[jj-1]:
+                number_commom_sequences[ii, jj]= min(max(number_commom_sequences[ii-1, jj]+1, number_commom_sequences[ii, jj-1]+1), min(ii, jj))
+            else:
+                number_commom_sequences[ii, jj]= max(number_commom_sequences[ii-1, jj], number_commom_sequences[ii, jj-1])
+    return number_commom_sequences[a_len, b_len]
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    #input = sys.stdin.read()
+    data = list(map(int, input().split()))
 
     n = data[0]
     data = data[1:]
