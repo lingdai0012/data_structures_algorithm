51f4e86c05c850e7d872f44890e2e2aef2bccea5
lingdai0012 <ling_dai@outlook.com>
2022-04-11 18:40:48 +0200

    add new solutions

--- Course1_Algorithm_Toolbox/programming_assignments/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ Course1_Algorithm_Toolbox/programming_assignments/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,15 +1,39 @@
 # Uses python3
 import sys
 
+
 def optimal_weight(W, w):
     # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    results = [0]
+    remaining_gold = [w]
+    for capacity in range(1, W + 1):
+        maximum_weight = 0
+        usable_gold = w
+        for gold in w:
+            if capacity < gold:
+                continue
+            else:
+                if gold in remaining_gold[capacity - gold]:
+                    if gold + results[capacity - gold] > maximum_weight:
+                        maximum_weight = max(
+                            maximum_weight, gold + results[capacity - gold]
+                        )
+                        usable_gold = remaining_gold[capacity - gold].copy()
+                        usable_gold.remove(gold)
+                    else:
+                        continue
+                else:
+                    if results[capacity - gold] > maximum_weight:
+                        maximum_weight = max(maximum_weight, results[capacity - gold])
+                        usable_gold = remaining_gold[capacity - gold]
+                    else:
+                        continue
+        results.append(maximum_weight)
+        remaining_gold.append(usable_gold)
+    return results[-1]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
     print(optimal_weight(W, w))
