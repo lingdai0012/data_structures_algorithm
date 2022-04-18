136664e636fe143c9408bf45d4222b9f4e1b2010
lingdai0012 <ling_dai@outlook.com>
2022-04-19 03:41:51 +0700

    best solutions

--- Course1_Algorithm_Toolbox/programming_assignments/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
+++ Course1_Algorithm_Toolbox/programming_assignments/week6_dynamic_programming2/3_maximum_value_of_an_arithmetic_expression/placing_parentheses.py
@@ -1,4 +1,8 @@
 # Uses python3
+import numpy as np
+import math
+
+
 def evalt(a, b, op):
     if op == "+":
         return a + b
@@ -10,8 +14,36 @@ def evalt(a, b, op):
         assert False
 
 
+# def get_maximum_value(dataset):
+#     # write your code here
+#     numbers = []
+#     symbols = []
+#     for ii in range(len(dataset)):
+#         if ii % 2 == 0:
+#             numbers.append(int(dataset[ii]))
+#         else:
+#             symbols.append(dataset[ii])
+
+#     def top_down_dp(numbers, symbols):
+#         if len(numbers) == 1:
+#             return numbers[0]
+#         max_result = 0
+#         for ii in range(len(symbols)):
+#             temp_result = top_down_dp(
+#                 numbers[:ii]
+#                 + [evalt(numbers[ii], numbers[ii + 1], symbols[ii])]
+#                 + numbers[ii + 2 :],
+#                 symbols[:ii] + symbols[ii + 1 :],
+#             )
+#             if temp_result > max_result:
+#                 max_result = temp_result
+#         return max_result
+
+#     return top_down_dp(numbers, symbols)
+
+
 def get_maximum_value(dataset):
-    # write your code here
+
     numbers = []
     symbols = []
     for ii in range(len(dataset)):
@@ -20,22 +52,32 @@ def get_maximum_value(dataset):
         else:
             symbols.append(dataset[ii])
 
-    def top_down_dp(numbers, symbols):
-        if len(numbers) == 1:
-            return numbers[0]
-        max_result = 0
-        for ii in range(len(symbols)):
-            temp_result = top_down_dp(
-                numbers[:ii]
-                + [evalt(numbers[ii], numbers[ii + 1], symbols[ii])]
-                + numbers[ii + 2 :],
-                symbols[:ii] + symbols[ii + 1 :],
-            )
-            if temp_result > max_result:
-                max_result = temp_result
-        return max_result
-
-    return top_down_dp(numbers, symbols)
+    M = np.zeros(shape=(len(numbers), len(numbers)), dtype=int)
+    m = np.zeros(shape=(len(numbers), len(numbers)), dtype=int)
+    for ii in range(len(numbers)):
+        M[ii, ii] = numbers[ii]
+        m[ii, ii] = numbers[ii]
+        if ii < len(numbers) - 1:
+            M[ii, ii + 1] = evalt(numbers[ii], numbers[ii + 1], symbols[ii])
+            m[ii, ii + 1] = evalt(numbers[ii], numbers[ii + 1], symbols[ii])
+    for jj in range(2, len(numbers)):
+        for ii in range(jj - 1, -1, -1):
+            max_val = -math.inf
+            min_val = math.inf
+            for k in range(ii, jj):
+                possible_results = [
+                    evalt(m[ii, k], m[k + 1, jj], symbols[k]),
+                    evalt(M[ii, k], M[k + 1, jj], symbols[k]),
+                    evalt(M[ii, k], m[k + 1, jj], symbols[k]),
+                    evalt(m[ii, k], M[k + 1, jj], symbols[k]),
+                ]
+                if max(possible_results) > max_val:
+                    max_val = max(possible_results)
+                if min(possible_results) < min_val:
+                    min_val = min(possible_results)
+            M[ii, jj] = max_val
+            m[ii, jj] = min_val
+    return M[0, -1]
 
 
 if __name__ == "__main__":
