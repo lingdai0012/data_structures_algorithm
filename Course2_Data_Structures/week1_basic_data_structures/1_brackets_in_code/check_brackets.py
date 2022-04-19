05b6d135922369aef65058f2b91d739c20ba708a
lingdai0012 <ling_dai@outlook.com>
2022-04-19 07:24:13 +0700

    implementation of stack in python

--- Course2_Data_Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ Course2_Data_Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -5,26 +5,50 @@ from collections import namedtuple
 Bracket = namedtuple("Bracket", ["char", "position"])
 
 
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
+class Stack(object):
+    def __init__(self):
+        self.__items = []
+
+    def is_empty(self):
+        return self.__items == []
+
+    def push(self, key):
+        self.__items.append(key)
+
+    def pop(self):
+        return self.__items.pop()
+
+    def peek(self):
+        return self.__items[-1]
 
 
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
-        if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+    stack = Stack()
+    left_brackets = ["[", "{", "("]
+    right_brackets = ["]", "}", ")"]
+    for ii in range(len(text)):
+        if text[ii] in left_brackets:
+            stack.push(Bracket(text[ii], ii))
+        else:
+            if text[ii] in right_brackets:
+                if stack.is_empty():
+                    return ii + 1
+                top = stack.pop()
+                if left_brackets[right_brackets.index(text[ii])] != top[0]:
+                    return ii + 1
+    if stack.is_empty():
+        return "Success"
+    else:
+        while not stack.is_empty():
+            first_item = stack.pop()
+        return first_item[1] + 1
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
