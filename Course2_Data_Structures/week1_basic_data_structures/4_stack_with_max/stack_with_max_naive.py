d6f983298a2b5c8dd39a62db7e157e3db3ce3e3c
lingdai0012 <ling_dai@outlook.com>
2022-04-23 00:48:14 +0700

    final solution

--- Course2_Data_Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ Course2_Data_Structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -2,34 +2,31 @@
 import sys
 import math
 
-def binary_search
 
 class StackWithMax:
     def __init__(self):
         self.__stack = []
-        self.__sorted_stack_index = []
+        self.__maxs = []
 
     def Push(self, a):
         self.__stack.append(a)
-        new_index = self.size()-1
-        
-
+        if len(self.__maxs) == 0:
+            self.__maxs.append(a)
+        else:
+            if a > self.__maxs[-1]:
+                self.__maxs.append(a)
+            else:
+                self.__maxs.append(self.__maxs[-1])
 
     def Pop(self):
         assert len(self.__stack)
-        last_element = self.__stack.pop()
-        if last_element  == self.__max:
-            if self.__max_num > 1:
-                self.__max_num -= 1
-            else:
-                self.
+        self.__stack.pop()
+        self.__maxs.pop()
 
     def Max(self):
         assert len(self.__stack)
-        return max(self.__stack)
+        return self.__maxs[-1]
 
-    def size(self):
-        return len(self.__stack)
 
 if __name__ == "__main__":
     stack = StackWithMax()
--- Course2_Data_Structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ Course2_Data_Structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,18 +1,105 @@
 # python3
+import math
+from collections import namedtuple
+
+
+class Stack(object):
+    def __init__(self):
+        self.__items = []
+
+    def push(self, key):
+        self.__items.append(key)
+
+    def pop(self):
+        return self.__items.pop()
+
+    def size(self):
+        return len(self.__items)
+
+    def is_empty(self):
+        return self.__items == []
+
+    def peek(self):
+        return self.__items[-1]
+
+
+class Queue(object):
+    def __init__(self):
+        self.__s1 = Stack()
+        self.__s2 = Stack()
+        self.__max_s1 = []
+        self.__max_s2 = []
+
+    def enqueue(self, key):
+        self.__s2.push(key)
+        if len(self.__max_s2) == 0:
+            self.__max_s2.append(key)
+        else:
+            if key > self.__max_s2[-1]:
+                self.__max_s2.append(key)
+            else:
+                self.__max_s2.append(self.__max_s2[-1])
+
+    def dequeue(self):
+        if self.__s1.is_empty():
+            while not self.__s2.is_empty():
+                recent_items = self.__s2.pop()
+                self.__s1.push(recent_items)
+                if self.__max_s1 == []:
+                    self.__max_s1.append(recent_items)
+                else:
+                    if recent_items > self.__max_s1[-1]:
+                        self.__max_s1.append(recent_items)
+                    else:
+                        self.__max_s1.append(self.__max_s1[-1])
+
+            self.__max_s2 = []
+        popped_item = self.__s1.pop()
+        self.__max_s1 = self.__max_s1[:-1]
+        return popped_item
+
+    def peek(self):
+        return self.__s1.peek()
+
+    def is_empty(self):
+        return self.__s1.is_empty()
+
+    def size(self):
+        return self.__s1.size()
+
+    def max(self):
+        if len(self.__max_s1) == 0:
+            return self.__max_s2[-1]
+        if len(self.__max_s2) == 0:
+            return self.__max_s1[-1]
+        return max(self.__max_s2[-1], self.__max_s1[-1])
+
+
+def max_sliding_window(sequence, m):
+    maximums = []
+    queue = Queue()
+    for ii in range(m - 1):
+        queue.enqueue(sequence[ii])
+
+    for ii in range(m - 1, len(sequence)):
+        queue.enqueue(sequence[ii])
+        maximums.append(queue.max())
+        _ = queue.dequeue()
+    return maximums
 
 
 def max_sliding_window_naive(sequence, m):
     maximums = []
     for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+        maximums.append(max(sequence[i : i + m]))
 
     return maximums
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     n = int(input())
     input_sequence = [int(i) for i in input().split()]
     assert len(input_sequence) == n
     window_size = int(input())
 
-    print(*max_sliding_window_naive(input_sequence, window_size))
-
+    print(*max_sliding_window(input_sequence, window_size))
