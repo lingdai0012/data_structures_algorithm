e390702a0d8365edb32908407db06870835e9bee
lingdai0012 <ling_dai@outlook.com>
2022-04-28 13:11:30 +0700

    correct methods

--- Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
+++ Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.py
@@ -1,5 +1,6 @@
 # python3
 import math
+import sys
 
 
 def build_heap(data):
@@ -19,21 +20,46 @@ def build_heap(data):
     #         if data[i] > data[j]:
     #             swaps.append((i, j))
     #             data[i], data[j] = data[j], data[i]
-    for ii in range(len(data) - 1, -1, -1):
-        sift_up(ii, data, swaps)
+    for ii in range(len(data) // 2, -1, -1):
+        sift_down(ii, data, swaps)
     return swaps
 
 
-def sift_up(ii, data, swaps):
-    if ii == 0:
-        return
-    parent = (ii - 1) // 2
-    if data[parent] > data[ii]:
-        data[parent], data[ii] = data[ii], data[parent]
-        swaps.append((parent, ii))
-        sift_up(parent, data, swaps)
+# def sift_up(ii, data, swaps):
+#     if ii == 0:
+#         return
+#     parent = (ii - 1) // 2
+#     if data[parent] > data[ii]:
+#         data[parent], data[ii] = data[ii], data[parent]
+#         swaps.append((parent, ii))
+#         sift_up(parent, data, swaps)
+#     else:
+#         return
+
+
+def sift_down(ii, data, swaps):
+    minimum_index = ii
+    if 2 * ii + 1 < len(data) and data[2 * ii + 1] < data[minimum_index]:
+        minimum_index = 2 * ii + 1
+
+    if 2 * ii + 2 < len(data) and data[2 * ii + 2] < data[minimum_index]:
+        minimum_index = 2 * ii + 2
+
+    if minimum_index != ii:
+        data[ii], data[minimum_index] = data[minimum_index], data[ii]
+        swaps.append((ii, minimum_index))
+        sift_down(minimum_index, data, swaps)
     else:
         return
+
+
+def check_is_heap(data):
+    for ii in range(len(data) - 1, -1, -1):
+        if ii == 0:
+            return True
+        else:
+            if data[(ii - 1) // 2] > data[ii]:
+                return False
 
 
 def main():
