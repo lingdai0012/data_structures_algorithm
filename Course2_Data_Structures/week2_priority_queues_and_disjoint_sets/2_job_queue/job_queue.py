2ea0ca021dfd26d5cfb517de57a99b82d8173405
lingdai0012 <ling_dai@outlook.com>
2022-04-30 23:32:49 +0200

    correct answer

--- Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -5,81 +5,45 @@ from collections import namedtuple
 AssignedJob = namedtuple("AssignedJob", ["worker", "started_at", "ended_at"])
 
 
-class Binary_Complete_Tree:
-    def __init__(self):
-        self.__nodes = []
+def sift_up(workers: list, index: int):
+    if index == 0:
+        return
+    parent = (index - 1) // 2
+    if (workers[parent].ended_at > workers[index].ended_at) or (
+        (workers[parent].ended_at == workers[index].ended_at)
+        and (workers[parent].worker > workers[index].worker)
+    ):
+        workers[parent], workers[index] = (
+            workers[index],
+            workers[parent],
+        )
+        sift_up(workers, parent)
+    else:
+        return
 
-    def insert(self, node: AssignedJob):
-        self.__nodes.append(node)
-        self.sift_up(len(self.__nodes) - 1)
 
-    def sift_up(self, index: int):
-        if index == 0:
-            return
-        parent = (index - 1) // 2
-        if self.__nodes[parent].ended_at > self.__nodes[index].ended_at:
-            self.__nodes[parent], self.__nodes[index] = (
-                self.__nodes[index],
-                self.__nodes[parent],
-            )
-        else:
-            return
-
-    def sift_down(self, ii: int):
-        minimum_index = ii
-        if (
-            2 * ii + 1 < len(self.__nodes)
-            and self.__nodes[2 * ii + 1].ended_at < self.__nodes[minimum_index].ended_at
+def sift_down(workers: list, index: int):
+    minimum_index = index
+    if 2 * index + 1 > len(workers) - 1:
+        return
+    else:
+        if (workers[2 * index + 1].ended_at < workers[minimum_index].ended_at) or (
+            workers[2 * index + 1].ended_at == workers[minimum_index].ended_at
+            and workers[2 * index + 1].worker < workers[minimum_index].worker
         ):
-            minimum_index = 2 * ii + 1
+            minimum_index = 2 * index + 1
 
-        if (
-            2 * ii + 2 < len(self.__nodes)
-            and self.__nodes[2 * ii + 2].ended_at < self.__nodes[minimum_index].ended_at
+    if 2 * index + 2 <= len(workers) - 1:
+        if (workers[2 * index + 2].ended_at < workers[minimum_index].ended_at) or (
+            workers[2 * index + 2].ended_at == workers[minimum_index].ended_at
+            and workers[2 * index + 2].worker < workers[minimum_index].worker
         ):
-            minimum_index = 2 * ii + 2
+            minimum_index = 2 * index + 2
 
-        if minimum_index != ii:
-            self.__nodes[ii], self.__nodes[minimum_index] = (
-                self.__nodes[minimum_index],
-                self.__nodes[ii],
-            )
-            self.sift_down(minimum_index)
-        else:
-            return
-
-    def extract_minimum(self):
-        if self.is_empty():
-            return
-        else:
-            latest_node = self.__nodes[0]
-            self.__nodes[0] = self.__nodes[-1]
-            self.__nodes = self.__nodes[:-1]
-            self.sift_down(0)
-            return latest_node
-
-    def is_empty(self):
-        return self.__nodes == []
-
-    def show_nodes(self):
-        return self.__nodes
-
-
-# class Queue:
-#     def __init__(self):
-#         self.__items = []
-
-#     def enqueue(self, key):
-#         self.__nodes.append(key)
-
-#     def dequeue(self):
-#         return self.__nodes.pop(0)
-
-#     def is_empty(self):
-#         return self.__items == []
-
-#     def peek(self):
-#         return self.__items[0]
+    if minimum_index != index:
+        workers[minimum_index], workers[index] = workers[index], workers[minimum_index]
+        sift_down(workers, minimum_index)
+    return
 
 
 def assign_jobs(n_workers, jobs):
@@ -91,23 +55,17 @@ def assign_jobs(n_workers, jobs):
     #     result.append(AssignedJob(next_worker, next_free_time[next_worker]))
     #     next_free_time[next_worker] += job
     result = []
-    tree = Binary_Complete_Tree()
-    workers = list(range(n_workers))
+    workers = [AssignedJob(ii, 0, 0) for ii in range(n_workers)]
     for job in jobs:
-        if len(workers) > 0:
-            worker = workers.pop(0)
-            assignedJob = AssignedJob(worker, 0, job)
-            tree.insert(assignedJob)
-            result.append(assignedJob)
-        else:
-            quickiest_thread = tree.extract_minimum()
-            assignedJob = AssignedJob(
-                quickiest_thread.worker,
-                quickiest_thread.ended_at,
-                quickiest_thread.ended_at + job,
-            )
-            tree.insert(assignedJob)
-            result.append(assignedJob)
+        earliest_worker = workers[0]
+        assigned_worker = AssignedJob(
+            earliest_worker.worker,
+            earliest_worker.ended_at,
+            earliest_worker.ended_at + job,
+        )
+        workers[0] = assigned_worker
+        result.append(assigned_worker)
+        sift_down(workers, 0)
     return result
 
 
