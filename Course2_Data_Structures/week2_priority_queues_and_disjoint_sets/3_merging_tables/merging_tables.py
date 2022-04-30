a7fb16776353b69b26a840d300b3f498c8574fea
lingdai0012 <ling_dai@outlook.com>
2022-05-01 00:48:39 +0200

    disjoint set

--- Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ Course2_Data_Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -16,6 +16,32 @@ class Database:
         if src_parent == dst_parent:
             return False
 
+        if self.ranks[src_parent] > self.ranks[dst_parent]:
+            self.parents[dst_parent] = src_parent
+            self.row_counts[src_parent] = (
+                self.row_counts[src_parent] + self.row_counts[dst_parent]
+            )
+            self.row_counts[dst_parent] = 0
+        elif self.ranks[src_parent] < self.ranks[dst_parent]:
+            self.parents[src_parent] = dst_parent
+            self.row_counts[dst_parent] = (
+                self.row_counts[src_parent] + self.row_counts[dst_parent]
+            )
+            self.row_counts[src_parent] = 0
+        else:
+            self.parents[src_parent] = dst_parent
+            self.ranks[dst_parent] += 1
+            self.row_counts[dst_parent] = (
+                self.row_counts[src_parent] + self.row_counts[dst_parent]
+            )
+            self.row_counts[src_parent] = 0
+        if (
+            max(self.row_counts[dst_parent], self.row_counts[src_parent])
+            > self.max_row_count
+        ):
+            self.max_row_count = max(
+                self.row_counts[dst_parent], self.row_counts[src_parent]
+            )
         # merge two components
         # use union by rank heuristic
         # update max_row_count with the new maximum table size
@@ -23,6 +49,8 @@ class Database:
 
     def get_parent(self, table):
         # find parent and compress path
+        if self.parents[table] != table:
+            self.parents[table] = self.get_parent(self.parents[table])
         return self.parents[table]
 
 
