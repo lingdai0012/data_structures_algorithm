c254f5c9e19612ffd7eea9c4746a19913a700831
lingdai0012 <ling_dai@outlook.com>
2022-05-08 23:03:33 +0200

    hash_chains

--- Course2_Data_Structures/week3_hash_tables/1_phone_book/phone_book.py
+++ Course2_Data_Structures/week3_hash_tables/1_phone_book/phone_book.py
@@ -1,47 +1,56 @@
 # python3
 
+
 class Query:
     def __init__(self, query):
         self.type = query[0]
         self.number = int(query[1])
-        if self.type == 'add':
+        if self.type == "add":
             self.name = query[2]
+
+
+class PhoneBook:
+    def __init__(self):
+        self.__phone_book = {}
+
+    def add(self, query: Query):
+        self.__phone_book[query.number] = query.name
+
+    def delete(self, query: Query):
+        if self.find(query) != "not found":
+            self.__phone_book.pop(query.number)
+        else:
+            pass
+
+    def find(self, query: Query):
+        return self.__phone_book.get(query.number, "not found")
+
+    def get_phone_book(self):
+        return self.__phone_book
+
 
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
 
+
 def write_responses(result):
-    print('\n'.join(result))
+    print("\n".join(result))
+
 
 def process_queries(queries):
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    # # Keep list of all existing (i.e. not deleted yet) contacts.
+    contacts = PhoneBook()
     for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+        if cur_query.type == "add":
+            contacts.add(cur_query)
+        elif cur_query.type == "del":
+            contacts.delete(cur_query)
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            result.append(contacts.find(cur_query))
     return result
 
-if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
 
+if __name__ == "__main__":
+    write_responses(process_queries(read_queries()))
--- Course2_Data_Structures/week3_hash_tables/2_hash_chains/hash_chains.py
+++ Course2_Data_Structures/week3_hash_tables/2_hash_chains/hash_chains.py
@@ -1,10 +1,10 @@
 # python3
 
-class Query:
 
+class Query:
     def __init__(self, query):
         self.type = query[0]
-        if self.type == 'check':
+        if self.type == "check":
             self.ind = int(query[1])
         else:
             self.s = query[1]
@@ -17,7 +17,7 @@ class QueryProcessor:
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
         # store all strings in one list
-        self.elems = []
+        self.elems = [[]] * bucket_count
 
     def _hash_func(self, s):
         ans = 0
@@ -26,10 +26,10 @@ class QueryProcessor:
         return ans % self.bucket_count
 
     def write_search_result(self, was_found):
-        print('yes' if was_found else 'no')
+        print("yes" if was_found else "no")
 
     def write_chain(self, chain):
-        print(' '.join(chain))
+        print(" ".join(chain))
 
     def read_query(self):
         return Query(input().split())
@@ -37,28 +37,28 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(
+                # cur for cur in reversed(self.elems) if self._hash_func(cur) == query.ind
+                self.elems[query.ind]
+            )
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
+            hash_num = self._hash_func(query.s)
+            if query.type == "find":
+                self.write_search_result(query.s in self.elems[hash_num])
+            elif query.type == "add":
+                if query.s not in self.elems[hash_num]:
+                    self.elems[hash_num].insert(0, query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                if query.s in self.elems[hash_num]:
+                    self.elems[hash_num].remove(query.s)
 
     def process_queries(self):
         n = int(input())
         for i in range(n):
             self.process_query(self.read_query())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     bucket_count = int(input())
     proc = QueryProcessor(bucket_count)
     proc.process_queries()
