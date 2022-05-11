a1f6d94b424250c44c00e6d351ad9b160f654f0c
lingdai0012 <ling_dai@outlook.com>
2022-05-11 22:37:07 +0200

    course 3 graph

---
+++ Course3_Algorithms_on_graph/week1_graph_decomposition1/2_adding_exits_to_maze/connected_components.py
@@ -0,0 +1,40 @@
+# Uses python3
+
+import sys
+
+
+def number_of_components(adj):
+    result = 0
+    # write your code here
+    for ii in range(len(adj)):
+        if adj[ii] is not None:
+            result += 1
+            traverse(adj, ii)
+        else:
+            continue
+    return result
+
+
+def traverse(adj, ii):
+    if adj[ii] is None:
+        return
+    else:
+        adjacents = adj[ii]
+        adj[ii] = None
+        for vertice in adjacents:
+            traverse(adj, vertice)
+    return
+
+
+if __name__ == "__main__":
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
+    adj = [[] for _ in range(n)]
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    print(adj)
+    print(number_of_components(adj))
