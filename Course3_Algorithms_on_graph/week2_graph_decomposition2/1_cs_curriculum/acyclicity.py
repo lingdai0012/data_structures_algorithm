385884d2a6079998ef9d2d943c7c9dd6317cb269
lingdai0012 <ling_dai@outlook.com>
2022-05-13 21:40:58 +0200

    check cycle

--- Course3_Algorithms_on_graph/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
+++ Course3_Algorithms_on_graph/week2_graph_decomposition2/1_cs_curriculum/acyclicity.py
@@ -1,17 +1,36 @@
-#Uses python3
+# Uses python3
 
 import sys
 
 
 def acyclic(adj):
+    for ii in range(len(adj)):
+        if reachability(ii, ii, adj) == 1:
+            return 1
     return 0
 
-if __name__ == '__main__':
+
+def reachability(ii, target_ii, adj):
+    if adj[ii] is None:
+        return 0
+    else:
+        if target_ii in adj[ii]:
+            return 1
+        else:
+            nexts = adj[ii]
+            adj[ii] = None
+            for jj in nexts:
+                if reachability(jj, target_ii, adj) == 1:
+                    return 1
+    return 0
+
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
