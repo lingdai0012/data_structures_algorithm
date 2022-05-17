a98b256cd0fbbf50e77b2be3cd9cac3b4cf6f343
lingdai0012 <ling_dai@outlook.com>
2022-05-17 15:33:21 +0200

    bds algo

--- Course3_Algorithms_on_graph/week3_paths_in_graphs1/1_flight_segments/bfs.py
+++ Course3_Algorithms_on_graph/week3_paths_in_graphs1/1_flight_segments/bfs.py
@@ -1,18 +1,42 @@
-#Uses python3
+# Uses python3
 
 import sys
 import queue
+import math
+
 
 def distance(adj, s, t):
-    #write your code here
-    return -1
+    # write your code here
+    used = [0 for _ in range(len(adj))]
+    q = queue.Queue()
+    q.put((s, 0))
+    min_distances = []
+    used[s] = 1
+    bfs(adj, q, t, min_distances, used)
+    if min_distances == []:
+        return -1
+    else:
+        return min(min_distances)
 
-if __name__ == '__main__':
+
+def bfs(adj, q, t, min_distances, used):
+    while not q.empty():
+        first, distance = q.get()
+        for ii in adj[first]:
+            if ii == t:
+                min_distances.append(distance + 1)
+            if used[ii] != 1:
+                q.put((ii, distance + 1))
+                used[ii] = 1
+    return
+
+
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    edges = list(zip(data[0 : (2 * m) : 2], data[1 : (2 * m) : 2]))
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
