033f52deaf93eca8d6bd89451e7f5ab7f8717b3c
lingdai0012 <ling_dai@outlook.com>
2022-05-24 23:38:05 +0200

    common substring

--- Course2_Data_Structures/week3_hash_tables/4_substring_equality/substring_equality.py
+++ Course2_Data_Structures/week3_hash_tables/4_substring_equality/substring_equality.py
@@ -1,35 +1,43 @@
 # python3
 
+import time
 import sys
+import random
+
+
+def get_hash_sum(s, x, m):
+    n = len(s)
+    h = [0]
+    for i in range(1, n + 1):
+        val = (x * h[i - 1] % m + ord(s[i - 1]) % m) % m
+        h.append(val)
+    return h
+
+
+def get_hash(h, a, l, x, m):
+    return h[a + l] % m - (pow(x, l, m) * h[a] % m) % m
 
 
 class Solver:
     def __init__(self, s):
         self.s = s
-        self.m1 = 10e9 + 7
-        self.m2 = 10e9 + 9
-        self.x = 1
-        self.hashtable_1 = self.precompute(self.m1)
-        self.hashtable_2 = self.precompute(self.m2)
-
-    def precompute(self, m):
-        hashtable = [0]
-        for ii in range(len(self.s)):
-            hashtable.append((self.x * hashtable[ii] + ord(self.s[ii])) % m)
-        return hashtable
+        self.x = random.randint(1, pow(10, 9))
+        self.m1 = pow(10, 9) + 7  # prime number above 500K
+        self.m2 = pow(10, 9) + 9
+        self.h1 = get_hash_sum(s, self.x, self.m1)
+        self.h2 = get_hash_sum(s, self.x, self.m2)
 
     def ask(self, a, b, l):
-        hash_a_1 = self.hashtable_1[a + l] - self.x**l * self.hashtable_1[a]
-        hash_b_1 = self.hashtable_1[b + l] - self.x**l * self.hashtable_1[b]
-        hash_a_2 = self.hashtable_2[a + l] - self.x**l * self.hashtable_2[a]
-        hash_b_2 = self.hashtable_2[b + l] - self.x**l * self.hashtable_2[b]
-        if (
-            hash_a_1 % self.m1 == hash_b_1 % self.m1
-            and hash_a_2 % self.m2 == hash_b_2 % self.m2
-        ):
-            return s[a : a + l] == s[b : b + l]
-        else:
-            return False
+        hash1a = get_hash(self.h1, a, l, self.x, self.m1)
+        hash1b = get_hash(self.h1, b, l, self.x, self.m1)
+
+        hash2a = get_hash(self.h2, a, l, self.x, self.m2)
+        hash2b = get_hash(self.h2, b, l, self.x, self.m2)
+
+        return (
+            hash1a % self.m1 == hash1b % self.m1
+            and hash2a % self.m2 == hash2b % self.m2
+        )
 
 
 s = sys.stdin.readline()
--- Course2_Data_Structures/week3_hash_tables/5_longest_common_substring/common_substring.py
+++ Course2_Data_Structures/week3_hash_tables/5_longest_common_substring/common_substring.py
@@ -3,18 +3,136 @@
 import sys
 from collections import namedtuple
 
-Answer = namedtuple('answer_type', 'i j len')
+Answer = namedtuple("answer_type", "i j len")
+
+
+def get_hash_sum(string, x, m):
+    n = len(string)
+    h = [0]
+    for i in range(1, n + 1):
+        val = (x * h[i - 1] % m + ord(string[i - 1]) % m) % m
+        h.append(val)
+    return h
+
+
+def get_hash(hash_table, start, length, x, m):
+    return (
+        hash_table[start + length] % m - (pow(x, length, m) * hash_table[start] % m) % m
+    )
+
+
+def get_hash_k(hash_table, string, length, x, m):
+    return [
+        get_hash(hash_table, ii, length, x, m) for ii in range(len(string) + 1 - length)
+    ]
+
+
+def check_equality(
+    ii,
+    jj,
+    start,
+    end,
+    hash_table_s_1,
+    hash_table_t_2,
+    hash_table_s_2,
+    hash_table_t_1,
+    x,
+    m1,
+    m2,
+):
+    if start == end or start == end - 1:
+        return Answer(ii, jj, start)
+    if ii + (start + end) // 2 >= len(hash_table_s_1) or jj + (start + end) // 2 >= len(
+        hash_table_t_1
+    ):
+        return Answer(ii, jj, start)
+    new_hash_s_k_1 = get_hash(hash_table_s_1, ii, (start + end) // 2, x, m1)
+    new_hash_t_k_1 = get_hash(hash_table_t_1, jj, (start + end) // 2, x, m1)
+
+    new_hash_s_k_2 = get_hash(hash_table_s_2, ii, (start + end) // 2, x, m2)
+    new_hash_t_k_2 = get_hash(hash_table_t_2, jj, (start + end) // 2, x, m2)
+
+    if (
+        new_hash_s_k_1 % m1 == new_hash_t_k_1 % m1
+        and new_hash_s_k_2 % m2 == new_hash_t_k_2 % m2
+    ):
+        return check_equality(
+            ii,
+            jj,
+            (start + end) // 2,
+            end,
+            hash_table_s_1,
+            hash_table_t_1,
+            hash_table_s_2,
+            hash_table_t_2,
+            x,
+            m1,
+            m2,
+        )
+    else:
+        return check_equality(
+            ii,
+            jj,
+            start,
+            (start + end) // 2,
+            hash_table_s_1,
+            hash_table_t_2,
+            hash_table_s_2,
+            hash_table_t_2,
+            x,
+            m1,
+            m2,
+        )
+
 
 def solve(s, t):
-	ans = Answer(0, 0, 0)
-	for i in range(len(s)):
-		for j in range(len(t)):
-			for l in range(min(len(s) - i, len(t) - j) + 1):
-				if (l > ans.len) and (s[i:i+l] == t[j:j+l]):
-					ans = Answer(i, j, l)
-	return ans
+    m1 = pow(10, 9) + 7
+    m2 = pow(10, 9) + 9
+
+    x = 1
+
+    hash_table_s_1 = get_hash_sum(s, x, m1)
+    hash_table_t_1 = get_hash_sum(t, x, m1)
+
+    hash_table_s_2 = get_hash_sum(s, x, m2)
+    hash_table_t_2 = get_hash_sum(t, x, m2)
+
+    max_length = min([len(s), len(t)])
+    middle = max_length // 2
+
+    hash_table_s_middle_1 = get_hash_k(hash_table_s_1, s, middle, x, m1)
+    hash_table_t_middle_1 = get_hash_k(hash_table_t_1, t, middle, x, m1)
+
+    hash_table_s_middle_2 = get_hash_k(hash_table_s_2, s, middle, x, m2)
+    hash_table_t_middle_2 = get_hash_k(hash_table_t_2, t, middle, x, m2)
+
+    possible_equals = []
+    for ii in range(len(hash_table_s_middle_1)):
+        if hash_table_s_middle_1[ii] in hash_table_t_middle_1:
+            jj = hash_table_t_middle_1.index(hash_table_s_middle_1[ii])
+            if hash_table_s_middle_2[ii] % m2 == hash_table_t_middle_2[jj] % m2:
+                possible_equals.append(
+                    check_equality(
+                        ii,
+                        jj,
+                        middle,
+                        max_length,
+                        hash_table_s_1,
+                        hash_table_t_1,
+                        hash_table_s_2,
+                        hash_table_t_2,
+                        x,
+                        m1,
+                        m2,
+                    )
+                )
+    if len(possible_equals) == 0:
+        return Answer(0, 1, 0)
+    else:
+        return max(possible_equals, key=lambda x: x[-1])
+
 
 for line in sys.stdin.readlines():
-	s, t = line.split()
-	ans = solve(s, t)
-	print(ans.i, ans.j, ans.len)
+    s, t = line.split()
+    ans = solve(s, t)
+    print(ans.i, ans.j, ans.len)
