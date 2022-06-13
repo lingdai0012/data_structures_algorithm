b208eb76c9943dfa492c0cd4d0568d371f873480
lingdai0012 <ling_dai@outlook.com>
2022-06-13 22:48:45 +0200

    add initial point

--- Course3_Algorithms_on_graph/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ Course3_Algorithms_on_graph/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -2,37 +2,46 @@
 
 import sys
 import math
+import random
 from collections import namedtuple
 
 
 edge = namedtuple("edge", ["start", "end", "cost"])
+max_weight = 10**9
 
 
 def bellman_ford(dist, prev, edges):
-    updated_index = []
+    relaxed = False
     for edge in edges:
-        if dist[edge.end] > dist[edge.start] + edge.cost:
+        if (
+            dist[edge.start] != max_weight
+            and dist[edge.end] > dist[edge.start] + edge.cost
+        ):
             dist[edge.end] = dist[edge.start] + edge.cost
             prev[edge.end] = edge.start
-            updated_index.append(edge.end)
-    return dist, prev, updated_index
+            relaxed = True
+    return dist, prev, relaxed
 
 
 def negative_cycle(adj, cost):
     # write your code here
-    s = 0
+    adj.append([_ for _ in range(len(adj))])
+    cost.append([0 for _ in range(len(adj))])
+    s = len(adj) - 1
     edges = [
         edge(ii, adj[ii][jj], cost[ii][jj])
         for ii in range(len(adj))
         for jj in range(len(adj[ii]))
     ]
-    dist = [0 if _ == s else math.inf for _ in range(len(adj))]
+    dist = [0 if _ == s else max_weight for _ in range(len(adj))]
     prev = [None for _ in range(len(adj))]
     for _ in range(len(adj) - 1):
-        dist, prev, _ = bellman_ford(dist, prev, edges)
+        dist, prev, relaxed = bellman_ford(dist, prev, edges)
+        if not relaxed:
+            return 0
 
-    dist_last, prev_last, last_updated_index = bellman_ford(dist, prev, edges)
-    if len(last_updated_index) != 0:
+    dist_last, prev_last, relaxed_last = bellman_ford(dist, prev, edges)
+    if relaxed_last:
         return 1
     return 0
 
