2ddd24b1e12f522d06ed03bea8ea87de7b72915c
lingdai0012 <ling_dai@outlook.com>
2022-06-13 20:28:49 +0200

    negative cycle

--- Course3_Algorithms_on_graph/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
+++ Course3_Algorithms_on_graph/week4_paths_in_graphs2/2_detecting_anomalies/negative_cycle.py
@@ -1,20 +1,51 @@
-#Uses python3
+# Uses python3
 
 import sys
+import math
+from collections import namedtuple
+
+
+edge = namedtuple("edge", ["start", "end", "cost"])
+
+
+def bellman_ford(dist, prev, edges):
+    updated_index = []
+    for edge in edges:
+        if dist[edge.end] > dist[edge.start] + edge.cost:
+            dist[edge.end] = dist[edge.start] + edge.cost
+            prev[edge.end] = edge.start
+            updated_index.append(edge.end)
+    return dist, prev, updated_index
 
 
 def negative_cycle(adj, cost):
-    #write your code here
+    # write your code here
+    s = 0
+    edges = [
+        edge(ii, adj[ii][jj], cost[ii][jj])
+        for ii in range(len(adj))
+        for jj in range(len(adj[ii]))
+    ]
+    dist = [0 if _ == s else math.inf for _ in range(len(adj))]
+    prev = [None for _ in range(len(adj))]
+    for _ in range(len(adj) - 1):
+        dist, prev, _ = bellman_ford(dist, prev, edges)
+
+    dist_last, prev_last, last_updated_index = bellman_ford(dist, prev, edges)
+    if len(last_updated_index) != 0:
+        return 1
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
+    edges = list(
+        zip(zip(data[0 : (3 * m) : 3], data[1 : (3 * m) : 3]), data[2 : (3 * m) : 3])
+    )
+    data = data[3 * m :]
     adj = [[] for _ in range(n)]
     cost = [[] for _ in range(n)]
     for ((a, b), w) in edges:
