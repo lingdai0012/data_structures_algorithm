2a441b984f534c543d7c8c1366e1e6378fb7a01d
lingdai0012 <ling_dai@outlook.com>
2022-06-14 00:32:13 +0200

    need a breadth first search algo to find all the points reachable from the one negative infinitive point

--- Course3_Algorithms_on_graph/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
+++ Course3_Algorithms_on_graph/week4_paths_in_graphs2/3_exchanging_money/shortest_paths.py
@@ -1,21 +1,61 @@
-#Uses python3
+# Uses python3
 
 import sys
 import queue
+from collections import namedtuple
+
+edge = namedtuple("edge", ["start", "end", "cost"])
+max_weight = 10**19
+
+
+def bellman_ford(distance, reachable, edges):
+    updated_index = []
+    for edge in edges:
+        if (
+            distance[edge.start] != max_weight
+            and distance[edge.end] > distance[edge.start] + edge.cost
+        ):
+            distance[edge.end] = distance[edge.start] + edge.cost
+            reachable[edge.end] = 1
+            updated_index.append(edge.end)
+    return updated_index
 
 
 def shortet_paths(adj, cost, s, distance, reachable, shortest):
-    #write your code here
-    pass
+    # write your code here
+    edges = [
+        edge(ii, adj[ii][jj], cost[ii][jj])
+        for ii in range(len(adj))
+        for jj in range(len(adj[ii]))
+    ]
+    reachable[s] = 1
+    distance[s] = 0
+    for _ in range(len(adj) - 1):
+        _updated_index = bellman_ford(distance, reachable, edges)
+        if _updated_index == []:
+            return
+
+    queue = bellman_ford(distance, reachable, edges)
+    visited = [False for _ in range(len(adj))]
+    while len(queue) != 0:
+        u = queue.pop(0)
+        shortest[u] = 0
+        visited[u] = True
+        for v in adj[u]:
+            if not visited[v] and v not in queue:
+                queue.append(v)
+    return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    data = data[3 * m:]
+    edges = list(
+        zip(zip(data[0 : (3 * m) : 3], data[1 : (3 * m) : 3]), data[2 : (3 * m) : 3])
+    )
+    data = data[3 * m :]
     adj = [[] for _ in range(n)]
     cost = [[] for _ in range(n)]
     for ((a, b), w) in edges:
@@ -23,15 +63,14 @@ if __name__ == '__main__':
         cost[a - 1].append(w)
     s = data[0]
     s -= 1
-    distance = [10**19] * n
+    distance = [max_weight] * n
     reachable = [0] * n
     shortest = [1] * n
     shortet_paths(adj, cost, s, distance, reachable, shortest)
     for x in range(n):
         if reachable[x] == 0:
-            print('*')
+            print("*")
         elif shortest[x] == 0:
-            print('-')
+            print("-")
         else:
             print(distance[x])
-
