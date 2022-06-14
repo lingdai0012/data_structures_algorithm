da79c95d4fb6f2c4cce63e53c85c1e52a04575ff
lingdai0012 <ling_dai@outlook.com>
2022-06-14 11:38:34 +0200

    clustering algo

--- Course3_Algorithms_on_graph/week5_spanning_trees/2_clustering/clustering.py
+++ Course3_Algorithms_on_graph/week5_spanning_trees/2_clustering/clustering.py
@@ -1,11 +1,51 @@
 # Uses python3
 import sys
 import math
+from collections import namedtuple
+import heapq
+
+point = namedtuple("point", ["x", "y"])
+edge = namedtuple("edge", ["weight", "point1", "point2"])
+
+
+def mean_sqaure_distance(point_1, point_2):
+    return math.sqrt((point_1.x - point_2.x) ** 2 + (point_1.y - point_2.y) ** 2)
+
+
+def find(x, parents):
+    if parents[x] == x:
+        return x
+    return find(parents[x], parents)
+
+
+def union(x, y, parents):
+    x_ancestor = find(x, parents)
+    y_ancestor = find(y, parents)
+    if x_ancestor <= y_ancestor:
+        parents[y_ancestor] = x_ancestor
+    else:
+        parents[x_ancestor] = y_ancestor
+    return parents
 
 
 def clustering(x, y, k):
     # write your code here
-    return -1.0
+    points = [point(x[ii], y[ii]) for ii in range(len(x))]
+    edges = [
+        edge(mean_sqaure_distance(points[ii], points[jj]), ii, jj)
+        for ii in range(len(x))
+        for jj in range(ii + 1, len(x))
+    ]
+    heapq.heapify(edges)
+    used_edges = []
+    parents = [_ for _ in range(len(points))]
+    while len(edges) != 0:
+        temp_edge = heapq.heappop(edges)
+        if find(temp_edge.point1, parents) != find(temp_edge.point2, parents):
+            parents = union(temp_edge.point1, temp_edge.point2, parents)
+            used_edges.append(temp_edge)
+
+    return round(used_edges[len(x) - k][0], 8)
 
 
 if __name__ == "__main__":
