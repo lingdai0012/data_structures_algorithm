f7c00622978993b647be1333b001e9caf3fc4ea5
lingdai <ling_dai@outlook.com>
2022-07-18 23:15:05 +0200

    black style

--- Course4_String_Algorithms/week1/non_shared_substring/non_shared_substring.py
+++ Course4_String_Algorithms/week1/non_shared_substring/non_shared_substring.py
@@ -1,13 +1,15 @@
 # python3
 import sys
 
-def solve (p, q):
-	result = p
-	return result
 
-p = sys.stdin.readline ().strip ()
-q = sys.stdin.readline ().strip ()
+def solve(p, q):
+    result = p
+    return result
 
-ans = solve (p, q)
 
-sys.stdout.write (ans + '\n')
+p = sys.stdin.readline().strip()
+q = sys.stdin.readline().strip()
+
+ans = solve(p, q)
+
+sys.stdout.write(ans + "\n")
