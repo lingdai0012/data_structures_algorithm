989891d299b3c1d88c9ea227dcf032a92adb11b7
Siqi Dai <daisiqi0401@gmail.com>
2022-12-21 00:48:22 +0100

    bwt and bwt inversion

--- Course4_String_Algorithms/week2/bwt/bwt.py
+++ Course4_String_Algorithms/week2/bwt/bwt.py
@@ -2,7 +2,14 @@
 import sys
 
 def BWT(text):
-    return ""
+    cyc_mat = cyclic_matrix(text)
+    cyc_mat = sorted(cyc_mat)
+    return "".join([row[-1] for row in cyc_mat])
+
+def cyclic_matrix(text: str) -> list:
+    return [text[ii:] + text[:ii] for ii in range(len(text))]
+
+
 
 if __name__ == '__main__':
     text = sys.stdin.readline().strip()
--- Course4_String_Algorithms/week2/bwtinverse/bwtinverse.py
+++ Course4_String_Algorithms/week2/bwtinverse/bwtinverse.py
@@ -1,11 +1,36 @@
 # python3
 import sys
+import time
 
 def InverseBWT(bwt):
     # write your code here
-    return ""
+    bwt_list, sorted_bwt_list, target = add_label(bwt)
+    last_to_first_map = {bwt_list[ii]:sorted_bwt_list[ii] for ii in range(len(bwt_list))}
+    original_text = []
+    while True:
+        original_text.append(bwt[target])
+        target = last_to_first_map[target]
+        if bwt[target] == "$":
+            break
+    return "".join(original_text[1:] + original_text[:1])
+
+def add_label(bwt: str) -> tuple:
+    occurrence = {}
+    element_dict = {"$": [], "A":[], "C":[], "G":[], "T":[]}
+    bwt_list= []
+    target_index = None
+    for ii in range(len(bwt)):
+        char = bwt[ii]
+        if char == "$":
+            target_index = ii
+        occurrence[char] = occurrence.get(char, 0) + 1
+        bwt_list.append(ii)
+        element_dict[char].append(ii)
+    sorted_bwt_list = element_dict["$"] + element_dict["A"] + element_dict["C"] + element_dict["G"] + element_dict["T"]
+    return bwt_list, sorted_bwt_list, target_index
 
 
 if __name__ == '__main__':
     bwt = sys.stdin.readline().strip()
     print(InverseBWT(bwt))
+    
